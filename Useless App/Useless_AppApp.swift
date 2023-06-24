ac0dfd43728e4a5b4d13e447b914a6488d00e672
Nothing <106498416+AlphaSolid@users.noreply.github.com>
2023-06-24 11:47:41 +0800

    Initial Commit

---
+++ Useless App/Useless_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Useless_AppApp.swift
+//  Useless App
+//
+//  Created by Marcus Low on 24/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Useless_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
