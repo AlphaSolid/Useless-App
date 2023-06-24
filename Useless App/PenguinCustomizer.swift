ae3f4b4fc03793afa716328b9fc33318b993249b
Advait "Conty" Contractor <advaitthepro@outlook.com>
2023-06-24 11:58:19 +0800

    Added resources

---
+++ Useless App/PenguinCustomizer.swift
@@ -0,0 +1,20 @@
+//
+//  PenguinCustomizer.swift
+//  Useless App
+//
+//  Created by Milind Contractor on 24/6/23.
+//
+
+import SwiftUI
+
+struct PenguinCustomizer: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct PenguinCustomizer_Previews: PreviewProvider {
+    static var previews: some View {
+        PenguinCustomizer()
+    }
+}
